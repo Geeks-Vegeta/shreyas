d1cec42ab4f6351c191680ba2116e0cd3ecaec6f
Geeks-Vegeta <shreyasmohite786@gmail.com>
2024-04-15 22:13:33 +0530

    rotos removed

---
+++ x.py
@@ -0,0 +1,30 @@
+# import pyjokes
+# print(pyjokes.get_joke())
+
+#output
+# Python is 👍
+# What's the object-oriented way to become wealthy? Inheritance.
+
+
+# import emoji
+# print(emoji.emojize('Python is :thumbs_up:'))
+
+
+# import antigravity
+# this will redirect you in the browser
+
+
+# from colorama import init, Fore
+# init(autoreset=True)
+# print("#output")
+# print(Fore.CYAN + 'some red text')
+# print('automatically back to default color again')
+
+
+
+import cowsay
+print("#output")
+cowsay.cow('Hello World')
+
+
+
